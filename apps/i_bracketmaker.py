6c30ce1b0fb6e6f37af27d79583b2822aeb1bd8e
Ken Harmon <harmkenn@gmail.com>
2024-02-09 07:20:02 +0100

    Ready

--- apps/i_bracketmaker.py
+++ apps/i_bracketmaker.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def app():
     # title of the app
-    py = 2023
+    py = 2024
     st.markdown('Predicting ' + str(py))
         
     fup = pd.read_csv("notebooks/step07_FUStats.csv").fillna(0)
@@ -46,7 +46,7 @@ def app():
         
     r1p = BBstats
     
-    pfs = LRF.predict(r1p[xcol]) + np.random.rand(32)*12-4
+    pfs = LRF.predict(r1p[xcol]) + np.random.rand(32)*0-0
     pus = RFU.predict(r1p[xcol]) 
     
 
@@ -70,7 +70,7 @@ def app():
     BBstats = BB[BB['Round']==2].merge(KBBP, left_on=['Year','PFTeam'],right_on=['Year','Team'],how='left')
     BBstats = BBstats.merge(KBBP, left_on=['Year','PUTeam'],right_on=['Year','Team'],how='left')
     
-    pfs = LRF.predict(BBstats[xcol]) + np.random.rand(16)*12-4
+    pfs = LRF.predict(BBstats[xcol]) + np.random.rand(16)*0-0
     pus = RFU.predict(BBstats[xcol])  
     for x in range(33,49):
         BB.loc[x,'PFScore']=pfs[x-33]
@@ -91,7 +91,7 @@ def app():
     BBstats = BB[BB['Round']==3].merge(KBBP, left_on=['Year','PFTeam'],right_on=['Year','Team'],how='left')
     BBstats = BBstats.merge(KBBP, left_on=['Year','PUTeam'],right_on=['Year','Team'],how='left')
     
-    pfs = LRF.predict(BBstats[xcol]) + np.random.rand(8)*12-4
+    pfs = LRF.predict(BBstats[xcol]) + np.random.rand(8)*0-0
     pus = RFU.predict(BBstats[xcol])  
     for x in range(49,57):
         BB.loc[x,'PFScore']=pfs[x-49]
@@ -112,7 +112,7 @@ def app():
     BBstats = BB[BB['Round']==4].merge(KBBP, left_on=['Year','PFTeam'],right_on=['Year','Team'],how='left')
     BBstats = BBstats.merge(KBBP, left_on=['Year','PUTeam'],right_on=['Year','Team'],how='left')
     
-    pfs = LRF.predict(BBstats[xcol]) + np.random.rand(4)*12-4
+    pfs = LRF.predict(BBstats[xcol]) + np.random.rand(4)*0-0
     pus = RFU.predict(BBstats[xcol])  
     for x in range(57,61):
         BB.loc[x,'PFScore']=pfs[x-57]
@@ -134,7 +134,7 @@ def app():
     BBstats = BB[BB['Round']==5].merge(KBBP, left_on=['Year','PFTeam'],right_on=['Year','Team'],how='left')
     BBstats = BBstats.merge(KBBP, left_on=['Year','PUTeam'],right_on=['Year','Team'],how='left')
     
-    pfs = LRF.predict(BBstats[xcol]) + np.random.rand(2)*12-4
+    pfs = LRF.predict(BBstats[xcol]) + np.random.rand(2)*0-0
     pus = RFU.predict(BBstats[xcol])  
     for x in range(61,63):
         BB.loc[x,'PFScore']=pfs[x-61]
@@ -155,7 +155,7 @@ def app():
     BBstats = BB[BB['Round']==6].merge(KBBP, left_on=['Year','PFTeam'],right_on=['Year','Team'],how='left')
     BBstats = BBstats.merge(KBBP, left_on=['Year','PUTeam'],right_on=['Year','Team'],how='left')
     
-    pfs = LRF.predict(BBstats[xcol]) + np.random.rand()*12-4
+    pfs = LRF.predict(BBstats[xcol]) + np.random.rand()*0-0
     pus = RFU.predict(BBstats[xcol])  
     for x in range(63,64):
         BB.loc[x,'PFScore']=pfs[x-63]
